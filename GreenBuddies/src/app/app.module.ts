814eefbccd29fae22e2715becd883c0eff9c493b
Lucila <lumonzon2021@gmail.com>
2022-07-05 01:03:12 -0300

    Creación de componententes Slider, listado (destacados y ofertas) y newsletter

--- GreenBuddies/src/app/app.module.ts
+++ GreenBuddies/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { MinilistadoComponent } from './minilistado/minilistado.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    MinilistadoComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent , SliderComponent]
 })
 export class AppModule { }
