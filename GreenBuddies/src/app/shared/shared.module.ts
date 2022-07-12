77d2e0c9c18ae38b8a8cb960221654889d63995e
Lucila <lumonzon2021@gmail.com>
2022-07-12 19:06:10 -0300

    Incorporacion del componente Newsletter al modulo shared

--- GreenBuddies/src/app/shared/shared.module.ts
+++ GreenBuddies/src/app/shared/shared.module.ts
@@ -1,38 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { RouterModule } from '@angular/router';
-
-@NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-  ]
-})
-
-=======
-import { MinilistadoComponent } from './minilistado/minilistado.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
-import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    MinilistadoComponent,
     NewsletterComponent,
-    SliderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    MinilistadoComponent,
     NewsletterComponent,
-    SliderComponent
   ]
 })
->>>>>>> 9e448275679ca9e074837a29c8e7a85fc453feab
+
 export class SharedModule { }
