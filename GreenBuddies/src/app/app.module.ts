283a78d57e60e01985b014f8f1ccb6b7795f2928
LizzBarreyro <lisabarreyro@gmail.com>
2022-07-12 23:59:18 -0300

    dasboard en produccion

--- GreenBuddies/src/app/app.module.ts
+++ GreenBuddies/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
+
   ],
   imports: [
     BrowserModule,
