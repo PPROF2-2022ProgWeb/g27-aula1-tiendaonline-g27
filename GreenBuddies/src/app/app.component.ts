fa413972685c5f3000c79d576db753c10134a213
Selien Yorbandi <selienyorbandi@gmail.com>
2022-07-20 18:43:55 -0300

    Scroll hasta arriba tras cambio de ruta

--- GreenBuddies/src/app/app.component.ts
+++ GreenBuddies/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GreenBuddies';
+  onActive() {
+    window.scroll(0, 0);
+  }
+
 }
