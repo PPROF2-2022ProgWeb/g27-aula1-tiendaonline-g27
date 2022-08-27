a25835f37b013acd6ba3d911679daa0c8f1e13d8
Lu <lulipsicounc@gmail.com>
2022-08-27 14:39:15 -0300

    Implementación de tests unitarios sobre la clase Product Service

---
+++ Backend/tienda/src/test/java/com/greenbuddies/store/service/ProductServiceTest.java
@@ -0,0 +1,107 @@
+package com.greenbuddies.store.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenbuddies.store.model.Product;
+import com.greenbuddies.store.repository.IProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ProductServiceTest {
+
+
+    private final Jackson2ObjectMapperBuilder JSON_BUILDER = Jackson2ObjectMapperBuilder.json();
+    private final ObjectMapper MAPPER = JSON_BUILDER.build();
+    List<Product> products = (MAPPER.readValue(new File("src/test/resources/mocked-objects/products.json"), new TypeReference<>() {
+    }));
+    IProductRepository iProductRepositoryMock = mock(IProductRepository.class);
+    ProductService productServiceTest = new ProductService(iProductRepositoryMock);
+
+
+    public ProductServiceTest() throws IOException {
+    }
+
+    @BeforeEach
+    public void setUp() {
+
+        when(iProductRepositoryMock.findAll()).thenReturn(products);
+
+        Optional<Product> productName = Optional.empty();
+        for (Product p : products) {
+            if (p.getName().equals("Porta Cepillo | Meraki")) {
+                productName = Optional.of(p);
+            }
+        }
+        when(iProductRepositoryMock.findProductByName("Porta Cepillo | Meraki")).thenReturn(productName);
+
+        Optional<Product> productId = Optional.empty();
+        for (Product p : products) {
+            if (p.getId().equals(2L)) {
+                productId = Optional.of(p);
+            }
+        }
+        when(iProductRepositoryMock.findById(2L)).thenReturn(productId);
+
+        List<Product> byCategoryProducts = new ArrayList<>();
+        for (Product p : products) {
+            if (p.getCategory().getName().equals("Cuidado personal")) {
+                byCategoryProducts.add(p);
+            }
+        }
+        when(iProductRepositoryMock.listProductsByCategory("Cuidado personal")).thenReturn(byCategoryProducts);
+    }
+
+    @Test
+    void findAll() throws IOException {
+        List<Product> products = productServiceTest.findAll();
+        assertTrue(products.size() > 0);
+    }
+
+    @Test
+    void findProductByName() {
+        Optional<Product> product = productServiceTest.findProductByName("Porta Cepillo | Meraki");
+        assertTrue(product.isPresent());
+        Assertions.assertEquals("Porta Cepillo | Meraki", product.get().getName());
+    }
+
+    @Test
+    void findById() {
+        Optional<Product> product = productServiceTest.findById(2L);
+        Assertions.assertEquals(2L, product.get().getId());
+    }
+
+    @Test
+    void listProductsByCategory() {
+        List<Product> personalCareProducts = productServiceTest.listProductsByCategory("Cuidado personal");
+        assertTrue(personalCareProducts.size() == 6);
+        personalCareProducts.forEach(System.out::println);
+
+    }
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+}
