bf1e7e0387d852ddfd64afef68bc264c9e1c29ad
CaroZTSDWeb <carozamudia@gmail.com>
2022-11-14 17:52:14 -0300

    blog version 2

---
+++ Frontend/GreenBuddies/src/app/services/post.service.ts
@@ -0,0 +1,29 @@
+//import { IApiArticles } from './../models/post.model';
+/*import { DOMAIN, PORT, PROTOCOL } from './infrastructure.properties';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+
+
+
+@Injectable({
+    providedIn:'root'
+})
+    
+export class PostService{
+
+private API_GET_ALL_ARTICLES = `${PROTOCOL}://${DOMAIN}:${PORT}/articles`;
+
+constructor(public http:HttpClient){}
+*/
+
+//método para llamar a todos los articles (o posts).
+
+/*public getAllArticles() : Observable<any> {
+    return this.http.get<any>('this.API_GET-ALL-ARTICLES');
+}
+
+    
+}*/
+
+
