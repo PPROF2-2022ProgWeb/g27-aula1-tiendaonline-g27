48a02b0c9575a072367cd6c6f31dbf2ebc646509
CaroZTSDWeb <carozamudia@gmail.com>
2022-11-18 22:12:29 -0300

    Form reactivo y validaciones TS: Contacto y arreglos en Blog

--- Frontend/GreenBuddies/src/app/pages/contacto/contacto.component.ts
+++ Frontend/GreenBuddies/src/app/pages/contacto/contacto.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
+
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({});
+  
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        name: ['', [Validators.compose([Validators.required, Validators.minLength(3),
+        Validators.maxLength(14), Validators.pattern('[a-zA-Z]*')])]],
+        lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(24),
+        Validators.pattern('[a-zA-Z]*')]],
+        email: ['', [Validators.compose([Validators.required, Validators.email])]],
+
+      })
   }
+
+  get name() {
+    return this.form?.get('name');
+  }
+
+  get nameValid() {
+    return this.name?.touched && !this.name?.valid;
+  }
+
+  get lastName() {
+    return this.form?.get('lastName');
+  }
+
+  get lastNameValid() {
+    return this.lastName?.touched && !this.lastName?.valid;
+  }
+
+  get email() {
+    return this.form?.get('email');
+  }
+
+  get emailValid() {
+    return this.email?.touched && !this.email?.valid;
+  }
+
+  onEnviar(event: Event) {
+
+    event.preventDefault;
+
+    if (this.form.valid) {
+      
+      alert("Su mensaje ha sido enviado")
+    }
+    else {
+
+      this.form.markAllAsTouched();
+
+    }
+
+  }
+
 
 }
